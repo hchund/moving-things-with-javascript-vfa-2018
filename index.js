0859e1a528c455e841261cfee5cd541404829156
Hannah Hund <hundxhc@gmail.com>
2018-06-19 21:58:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 var dodger = document.getElementById('dodger')
-      function moveDodgerRight(){
-        var rightNumbers = dodger.style.left.replace('px','')
-        var right = parseInt(rightNumbers, 10)
+var dodger = document.getElementById('dodger')
 
-        if(right < 360){
-          dodger.style.left = `${right + 1}px`
+    function moveDodgerRight() {
+      var rightNumbers = dodger.style.left.replace('px', '')
+      var right = parseInt(rightNumbers, 10)
+ 
+       if (right < 360) {
+         dodger.style.left = `${right + 1}px`
         }
       }
-
-      function moveDodgerLeft() {
-        var leftNumbers = dodger.style.left.replace('px', '')
-        var left = parseInt(leftNumbers, 10)
-
-        if (left > 0) {
-          dodger.style.left = `${left - 1}px`
-        }
+    document.addEventListener('keydown', function(e) {
+      if (e.which === 37) {
+        moveDodgerLeft()
       }
-      document.addEventListener('keydown', function(e){
-        if(e.which === 39){
-          moveDodgerRight()
-        }else if (e.which === 37) {
-          moveDodgerLeft()
-        }
-      })
+    })
